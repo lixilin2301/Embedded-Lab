0819bc5880eea3f67af59a0e950e0849c48eb5e5
misaelh <misael.leal@gmail.com>
2015-04-30 14:46:24 +0200

    Forgot matrix mult function

--- assignment1/src/gpp/matMult.c
+++ assignment1/src/gpp/matMult.c
@@ -114,6 +114,20 @@ extern "C"
 
   int *mat1, *mat2, *prod;
 
+  void matMult(int *mat1, int *mat2, int *prod, unsigned int size)
+  {
+    int i, j, k;
+    for (i = 0;i < size; i++)
+      {
+	for (j = 0; j < size; j++)
+	  {
+	    prod[size * i + j]=0;
+	    for(k=0;k<size;k++)
+	      prod[size * i + j] = prod[size * i + j]+mat1[size * i + k] * mat2[size * k + j];
+	  }
+      }
+  }
+
 #if defined (VERIFY_DATA)
     /** ============================================================================
      *  @func   helloDSP_VerifyData
